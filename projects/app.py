ce319822a4a2be4844d77677cb4dd1bca181d8d7
malik09-teach <malikabbaskhan477@gmail.com>
2026-04-10 22:01:30 +0500

    upt new model code

---
+++ projects/app.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import  accuracy_score
+from mlxtend.plotting import plot_decision_regions
+import pickle
+import streamlit as st
+
